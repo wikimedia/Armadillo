10b84f5422d75f2c7006e6de97de256d70acece8
Jon Robson <jdlrobson@gmail.com>
2023-04-25 17:15:10 -0700

    Containers are opaque until loaded

--- resources/armadillo.widgets/index.js
+++ resources/armadillo.widgets/index.js
@@ -1,5 +1,6 @@
 module.exports = {
     armadillo: function ( el, props ) {
         el.innerHTML = `<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b4/Nine-banded_Armadillo.jpg/800px-Nine-banded_Armadillo.jpg?20170615182855">`;
+        return Promise.resolve();
     }
 }
--- resources/armadillo/index.js
+++ resources/armadillo/index.js
@@ -1,11 +1,14 @@
 console.log( 'armadillo is running' );
 document.querySelectorAll('armadillo').forEach(( node ) => {
+    const container = node.parentNode;
     const module = node.dataset.module;
     const props = JSON.parse( node.dataset.props );
     mw.loader.using( module ).then( () => {
         require( module ).armadillo(
-            node.parentNode,
+            container,
             props
-        );
+        ).then(() => {
+            container.classList.add( 'armadillo-widget-loaded' );
+        })
     })
 });
